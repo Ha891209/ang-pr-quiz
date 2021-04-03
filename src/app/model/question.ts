42cb81ac6e4ac3926724e09f9797160456f77ee7
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-04-03 21:44:58 +0200

    model modified

--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -3,7 +3,7 @@ import { Answer } from "./answer";
 export class Question {
     id: number = 0;
     question: string = '';
-    answers: Answer[] = [];
+    answers: Answer[] = new Array;
     points: number = 0;
-    active: boolean = false;
+    active: boolean = true;
 }
--- src/app/model/quiz.ts
+++ src/app/model/quiz.ts
@@ -1,7 +1,9 @@
+import { Question } from "./question";
+
 export class Quiz {
     id: number = 0;
     title: string = '';
     description: string = '';
-    questions: number[] = [];
-    active: boolean = false;
+    questions: number[] = new Array;
+    active: boolean = true;
 }
