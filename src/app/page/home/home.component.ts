a49eb95c73d8b697da2d5b9daf810c1f073d95f7
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-04-04 13:30:44 +0200

    home modified

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Quiz } from 'src/app/model/quiz';
+import { QuizService } from 'src/app/service/quiz-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  quizzes$: Observable<Quiz[]> = this.quizService.getAll().pipe(
+    tap(
+      data => {
+        data.forEach(
+          item => {
+            item.qnums = item.questions.length;
+          })
+      })
+  );
+
+  constructor(private quizService: QuizService) { }
 
   ngOnInit(): void {
   }
 
 }
+
+
